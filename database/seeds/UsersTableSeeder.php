ca46714a1a369ecb99937cc3feffe21065a6476b
Yoel Diomedez <yoeldiomedez@gmail.com>
2020-04-12 09:52:25 -0500

    email admin changed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,7 +23,7 @@ class UsersTableSeeder extends Seeder
             'cmp'            => '134148',
             'position'       => 'Analista Programador de Sistemas',
             'specialty'      => 'Desarrollador de Sistemas de Información',
-            'email'          => 'admin@admin.com',
+            'email'          => 'yoeldiomedez@gmail.com',
             'password'       => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ]);
